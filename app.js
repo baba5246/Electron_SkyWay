c066ab575c66ef7d1a3b4dd290062edc94ca08af
baba5246 <baba_writer@yahoo.co.jp>
2022-06-02 02:12:23 +0900

    Fix a bug of node integration

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ app.on('ready', _ => {
         });
     });
 
-    win = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({
+        width: 800, height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
     win.loadURL('file://' + __dirname + '/window.html');
     //win.webContents.openDevTools();
     win.on('closed', _ => {
